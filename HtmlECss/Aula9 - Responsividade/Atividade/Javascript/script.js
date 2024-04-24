b7fe443411f23823b14456e6bbd8e216d30412a8
Felipe-Martins-De-Olveira <160751172+Felipe-Martins-De-Olveira@users.noreply.github.com>
2024-04-24 17:14:42 -0300

    Atividade atualização
    Atividade

---
+++ HtmlECss/Aula9 - Responsividade/Atividade/Javascript/script.js
@@ -0,0 +1,7 @@
+function btnMobilieTrocar() {
+    if (menuMobilie.style.display == "flex"){
+        menuMobilie.style.display= "none";
+    }else{
+        menuMobilie.style.display = "flex";
+    }
+}
