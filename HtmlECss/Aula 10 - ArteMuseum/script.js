cb0f3a2862b104b1250492778ab25ae5210b7fdc
Felipe-Martins-De-Olveira <160751172+Felipe-Martins-De-Olveira@users.noreply.github.com>
2024-05-03 15:06:27 -0300

    Atualização, fim do modulo HTML É CSS
    FIM DO MODULO HTLM É CSS

---
+++ HtmlECss/Aula 10 - ArteMuseum/script.js
@@ -0,0 +1,8 @@
+function btnMobile() {
+    var menuMobile = document.getElementById("menumobile");
+    if (menuMobile.style.display === "block") {
+        menuMobile.style.display = "none";
+    } else {
+        menuMobile.style.display = "block";
+    }
+}
